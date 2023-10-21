334b4672cfbdd3ec029c1c3a76c996fe48db5f3e
Peter Weiser <112249710+Pizzaboi87@users.noreply.github.com>
2023-10-21 16:28:31 +0300

    third practice

--- practice_03.py
+++ practice_03.py
@@ -20,6 +20,7 @@ print("------------")
 for key, value in capitals.items():
     print(key + " - " + value)
 
+
 #Function
 def test_function(first_name, last_name, age = 36):
     print ("Hello " + first_name + " " + last_name + "!")
@@ -35,3 +36,49 @@ def multiply(num1, num2):
     return num1 * num2
 
 print(multiply(4, 8))
+
+
+#Args
+def addition(*args):
+    sum = 0
+    for i in args:
+        sum += i
+    return sum
+
+print(addition(1,2,3,4,5))
+
+
+#Kwargs
+def hello(**kwargs):
+    print("Hello", end=" ")
+    for key, value in kwargs.items():
+        print(value, end=" ")
+
+hello(title="Ms.", first_name="Rahel", middle_name="Anna", last_name="Temesvari")
+
+
+#Format method
+animal = "cow"
+item = "moon"
+
+print("The {} jumped over the {}".format(animal, item))
+print("The {1} jumped over the {0}".format(animal, item))
+print("The {prop1} jumped over the {prop2}".format(prop1="dog", prop2="doghouse"))
+
+
+#Format method with positioning
+print("The result of the test is a(n) {result:<9} for Carlos.".format(result='"A"'))
+print("The result of the test is a(n) {result:>9} for Peter.".format(result='"C"'))
+print("The result of the test is a(n) {result:^9} for Putty.".format(result='"B'))
+
+
+#Format method with numbers
+PI = 3.14159
+number = 1000
+
+print("The PI to two decimal places: {:.2f}".format(PI))
+print("The number with comma: {:,}".format(number))
+print("The number in binary: {:b}".format(number))
+print("The number in octo: {:o}".format(number))
+print("The number in hexa: {:X}".format(number))
+print("The number in scientific: {:E}".format(number))
